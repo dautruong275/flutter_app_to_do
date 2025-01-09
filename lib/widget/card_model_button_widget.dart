e6255b2de20b5322f1dde299f40e02b3270d96aa
dautruong275 <dautruong.dt@gmail.com>
2025-01-09 15:27:48 +0700

    commit file

---
+++ lib/widget/card_model_button_widget.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class ModelButton extends StatelessWidget {
+  const ModelButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: MediaQuery.of(context).viewInsets,
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+          child: Column(
+            children: [
+              const TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Your task',
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              SizedBox(
+                width: double.infinity,
+                height: 50,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('Add Task'),
+                ),
+              ),
+            ],
+          ),
+        ));
+  }
+}
